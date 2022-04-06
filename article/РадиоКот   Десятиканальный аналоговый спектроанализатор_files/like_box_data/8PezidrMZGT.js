073ad3d48fa0fce0a1b6b291115b560fc8551255
OlegNaKone <ext.nkn@gmail.com>
2022-04-06 18:19:07 +0300

    added article for radiokot.ru

---
+++ article/РадиоКот   Десятиканальный аналоговый спектроанализатор_files/like_box_data/8PezidrMZGT.js
@@ -0,0 +1,6 @@
+/*!CK:3625984654!*//*1447097024,*/
+
+if (self.CavalryLogger) { CavalryLogger.start_js(["73jPk"]); }
+
+__d('IntlSlavicNumberType',['IntlVariations'],function a(b,c,d,e,f,g,h){if(c.__markCompiled)c.__markCompiled();var i={getNumberVariationType:function(j){var k=j%100,l=k%10;return l===1&&k!==11?h.NUMBER_SINGULAR:l>=2&&l<=4&&!(k>=12&&k<=14)?h.NUMBER_DUAL:h.NUMBER_PLURAL;}};f.exports=i;},null);
+__d('legacy:Pager',['Pager'],function a(b,c,d,e){if(c.__markCompiled)c.__markCompiled();b.Pager=c('Pager');},3);
